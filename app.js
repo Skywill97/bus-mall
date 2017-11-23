96d8e18ccadeee57436105f73994fbe06ef698c5
Skyler Williams <kaiser1997@gmail.com>
2017-11-22 17:17:51 -0800

    bussmallchartworking

--- app.js
+++ app.js
@@ -67,82 +67,59 @@ var tracker = {
         }
         tracker.displayImages();
         var ctx = document.getElementById('myChart').getContext('2d');
-     var options = {
-       type: 'bar',
-       data: {
-         labels: allProducts.map(function(x) {return x.name;}),
-         datasets: [{
-           label: '# of Votes',
-           data: allProducts.map(function(x) {return x.votes;}),
-           backgroundColor: [
-             'rgba(255, 99, 132, 0.2)',
-             'rgba(54, 162, 235, 0.2)',
-             'rgba(255, 206, 86, 0.2)',
-             'rgba(75, 192, 192, 0.2)',
-             'rgba(153, 102, 255, 0.2)',
-             'rgba(255, 159, 64, 0.2)',
-             'rgba(255, 99, 132, 0.2)',
-             'rgba(54, 162, 235, 0.2)',
-             'rgba(255, 206, 86, 0.2)',
-             'rgba(75, 192, 192, 0.2)',
-             'rgba(153, 102, 255, 0.2)',
-             'rgba(255, 159, 64, 0.2)',
-             'rgba(255, 99, 132, 0.2)',
-             'rgba(54, 162, 235, 0.2)',
-             'rgba(255, 206, 86, 0.2)',
-             'rgba(75, 192, 192, 0.2)',
-             'rgba(153, 102, 255, 0.2)',
-             'rgba(255, 159, 64, 0.2)',
-             'rgba(255, 99, 132, 0.2)',
-             'rgba(54, 162, 235, 0.2)',
-             'rgba(255, 206, 86, 0.2)',
-             'rgba(75, 192, 192, 0.2)',
-             'rgba(153, 102, 255, 0.2)',
-             'rgba(255, 159, 64, 0.2)'
-           ],
-           borderColor: [
-             'rgba(255,99,132,1)',
-             'rgba(54, 162, 235, 1)',
-             'rgba(255, 206, 86, 1)',
-             'rgba(75, 192, 192, 1)',
-             'rgba(153, 102, 255, 1)',
-             'rgba(255, 159, 64, 1)',
-             'rgba(255,99,132,1)',
-             'rgba(54, 162, 235, 1)',
-             'rgba(255, 206, 86, 1)',
-             'rgba(75, 192, 192, 1)',
-             'rgba(153, 102, 255, 1)',
-             'rgba(255, 159, 64, 1)',
-             'rgba(255,99,132,1)',
-             'rgba(54, 162, 235, 1)',
-             'rgba(255, 206, 86, 1)',
-             'rgba(75, 192, 192, 1)',
-             'rgba(153, 102, 255, 1)',
-             'rgba(255, 159, 64, 1)',
-             'rgba(255,99,132,1)',
-             'rgba(54, 162, 235, 1)',
-             'rgba(255, 206, 86, 1)',
-             'rgba(75, 192, 192, 1)',
-             'rgba(153, 102, 255, 1)',
-             'rgba(255, 159, 64, 1)'
-           ],
-           borderWidth: 1
-         },
-         ]
-       },
-       options: {
-         scales: {
-           yAxes: [{
-             ticks: {
-               beginAtZero: true
-             }
-           }]
-         }
-       }
-     };
-     var myChart = new Chart(ctx, options);
-     console.log('stuff',myChart);
-     tracker.imagesEl.removeEventListener('click', tracker.onClick);
+        var options = {
+          type: 'bar',
+          data: {
+            labels: allProducts.map(function(x) {return x.name;}),
+            datasets: [{
+              label: '# of Votes',
+              data: allProducts.map(function(x) {return x.votes;}),
+              backgroundColor: [
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)', ],
+              borderColor: [
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)',
+                'rgb(2, 14, 18)'
+              ],
+              borderWidth: 1
+            },
+            ]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            }
+          }
+        };
+        var myChart = new Chart(ctx, options);
+        console.log('stuff',myChart);
+        tracker.imagesEl.removeEventListener('click', tracker.onClick);
 
       }
     } else {
